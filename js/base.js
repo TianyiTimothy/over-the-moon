ea9611886f253b99d4d6711338fb0c86283eabb9
TianyiZhang <timothy.tudis@gmail.com>
2019-11-20 17:44:07 -0500

    first commit with module html

---
+++ js/base.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+	$("#header").html('<div class="container"><img id="logo" src="img/moon.png" alt="logo"/><ul><li><a href="#">Home</a></li><li><a href="#">Menu</a></li><li><a href="#">Site Map</a></li><li><a href="#">About Us</a></li></ul></div>');
+}
